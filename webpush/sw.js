5c88393636a5d65f7d54aba87cf9fda01ad72eb9
Roman Nix <rnix@yandex.ru>
2019-09-17 17:28:17 +0700

    Revert opening a new window instead of focusing

--- webpush/sw.js
+++ webpush/sw.js
@@ -23,7 +23,30 @@ self.addEventListener('notificationclick', function(event) {
     const data = event.notification.data;
 
     if (data.url) {
-        event.waitUntil(clients.openWindow(data.url));
+        const urlToOpen = new URL(data.url, self.location.origin).href;
+
+        const promiseChain = clients.matchAll({
+            type: 'window',
+            includeUncontrolled: true
+        })
+            .then((windowClients) => {
+                let matchingClient = null;
+
+                for (let i = 0; i < windowClients.length; i++) {
+                    const windowClient = windowClients[i];
+                    if (windowClient.url === urlToOpen) {
+                        matchingClient = windowClient;
+                        break;
+                    }
+                }
+
+                if (matchingClient) {
+                    return matchingClient.focus();
+                } else {
+                    return clients.openWindow(urlToOpen);
+                }
+            });
+
+        event.waitUntil(promiseChain);
     }
 });
-
