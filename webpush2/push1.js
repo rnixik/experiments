8fc605b1da93596f5bb58596926e77f9c1bd26be
Roman Nix <rnix@yandex.ru>
2019-09-17 12:29:50 +0700

    Update push1.js

--- webpush2/push1.js
+++ webpush2/push1.js
@@ -14,7 +14,7 @@ function initSW() {
     }
 
     //register the service worker
-    navigator.serviceWorker.register('sw.js')
+    navigator.serviceWorker.register('sw1.js')
         .then((serviceWorkerRegistration) => {
             console.log('serviceWorker installed1!');
             initPush(serviceWorkerRegistration);
