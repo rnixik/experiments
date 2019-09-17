63d48f976601fcbd7d853a0b29fcec7bf8a638c4
Roman Nix <rnix@yandex.ru>
2019-09-17 12:28:02 +0700

    Add files via upload

---
+++ webpush2/push1.js
@@ -0,0 +1,74 @@
+initSW();
+
+function initSW() {
+    if (!"serviceWorker" in navigator) {
+        //service worker isn't supported
+        return;
+    }
+
+    //don't use it here if you use service worker
+    //for other stuff.
+    if (!"PushManager" in window) {
+        //push isn't supported
+        return;
+    }
+
+    //register the service worker
+    navigator.serviceWorker.register('sw.js')
+        .then((serviceWorkerRegistration) => {
+            console.log('serviceWorker installed1!');
+            initPush(serviceWorkerRegistration);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function initPush(serviceWorkerRegistration) {
+    new Promise(function (resolve, reject) {
+        const permissionResult = Notification.requestPermission(function (result) {
+            resolve(result);
+        });
+
+        if (permissionResult) {
+            permissionResult.then(resolve, reject);
+        }
+    })
+        .then((permissionResult) => {
+            if (permissionResult !== 'granted') {
+                throw new Error('We weren\'t granted permission.');
+            }
+            subscribeUser(serviceWorkerRegistration);
+        });
+}
+
+function subscribeUser(serviceWorkerRegistration) {
+    const subscribeOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+            'BG1aEfDXrGVx11G4B9mxb-2nHSLtvxNwGJkn-mqOmz9aanUycak4IPMvvPFimxu_5vZGufaZ-aO0lu3BIbfKWnU'
+        )
+    };
+
+    serviceWorkerRegistration.pushManager.subscribe(subscribeOptions)
+        .then((pushSubscription) => {
+            console.log('Received PushSubscription1: ', JSON.stringify(pushSubscription));
+            document.getElementById('log1').value = JSON.stringify(pushSubscription);
+        });
+}
+
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
---
+++ webpush2/sw2.js
@@ -0,0 +1,18 @@
+self.addEventListener('push', function (e) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        //notifications aren't supported or permission not granted!
+        return;
+    }
+
+    if (e.data) {
+        var msg = e.data.json();
+        console.log(msg);
+        e.waitUntil(self.registration.showNotification(msg.title, {
+            body: msg.body,
+            icon: msg.icon,
+            data: msg.data,
+            actions: msg.actions
+        }));
+    }
+});
+
